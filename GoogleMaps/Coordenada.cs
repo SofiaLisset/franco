2c6842d494f366109633ee490df90ab70b9de21e
Sofia <sofmorchi@alu.edu.gva.es>
2025-01-25 09:48:58 +0100

    Creacion de las clases Coordenada y vehiculo , atributos, constructores y metodos

---
+++ GoogleMaps/Coordenada.cs
@@ -0,0 +1,37 @@
+﻿using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+
+class Coordenada{           /*Clase*/
+
+        private int X;          /*Atributos*/
+        private int Y;
+
+        public Coordenada (){          /*Constructor por defecto*/
+            X = 0;
+            Y = 0;
+        }
+
+        public Coordenada (int X, int Y){      /*Constructor sobrecarga*/
+            
+            this.X = X;
+            this.Y = Y;
+           
+
+        }
+
+        public int GetX (){     /*Metodos*/
+            return X;
+        }
+
+        public int GetY (){     
+            return Y;
+        }
+        public void SetX(int X){             
+           this.X = X;
+        }
+        
+        public void SetY(int Y){
+           this.Y = Y;
+        }
+
+}
---
+++ GoogleMaps/Vehiculo.cs
@@ -0,0 +1,18 @@
+class Vehiculo{
+        private  string Motor;
+        private  string Rueda;
+        private  float Velocidad;
+        private     Coordenada Posicion;
+
+        Vehiculo (){
+
+            Motor = "Generico";
+            Rueda = "Generico";
+            Velocidad = 0.0f;
+            Posicion = new Coordenada (0,0);
+
+        }
+
+
+}
+
